6199434bb8254c3cea116bb1d337126b0f0f160e
Giovani Rodrigues <giovani.rodrigues@finops2rp.rf.gd>
2025-01-29 17:51:50 +0000

    remove dotenv

--- dataform/dataform.py
+++ dataform/dataform.py
@@ -1,24 +1,22 @@
 from google.cloud import dataform_v1beta1
 from googleapiclient.discovery import build
 from google.cloud.dataform_v1beta1 import types
-from dotenv import dotenv_values
 from files import load_files, get_content
 from auth import get_auth
 
-config = dotenv_values(".env")
-
 # Secret 
-secret = config["SERVICE_ACCOUNT_KEY"]
+secret = "pfs-risco-tivea-key"
 
 # Project Vars
-project_id = config["PROJECT_ID"]
-project_number =config["PROJECT_NUMBER"]
-location = config["LOCATION"]
+project_id = "integracaohomologado"
+project_number = "95663131429"
+location = "southamerica-east1"
+
 parent = f'projects/{project_id}/locations/{location}'
 
 # Dataform vars
-repository = config["REPOSITORY"]
-workspace_name = config["WORKSPACE_NAME"]
+repository = "scripts_producao"
+workspace_name = "production"
 workspace_path = f"{parent}/repositories/{repository}/workspaces/{workspace_name}"
 
 print()
