62dad6c4e4fbfc0d8f3c928c10473822f9f4c752
Henrique Fernandes <henriquealcalde@hotmail.com>
2025-02-04 09:47:16 -0300

    feat:add dataform auth and commands in cloudbuild

--- dataform/auth.py
+++ dataform/auth.py
@@ -1,44 +1,29 @@
-from google.cloud import secretmanager
-import json
-from google.auth import credentials
-from google.oauth2 import service_account
-
-'''
-A função 'acess_secret' tem o objetivo de obter a secret armazenada no Secret Manager. 
-Passamos a número do projeto e o id da secret para obtermnos a secret.
-EX:
-    service_account_json = access_secret(project_number, secret_id)
-'''
-
-def access_secret(project_number, secret_id):
-    client = secretmanager.SecretManagerServiceClient()
-    secret_name = f"projects/{project_number}/secrets/{secret_id}/versions/latest"
-    response = client.access_secret_version(name=secret_name)
-    secret_payload = response.payload.data.decode("UTF-8")
-    return secret_payload
-
+from google.auth import exceptions
+from google.auth.transport.requests import Request
+from google.auth import default
 
 '''
 A função 'get_auth' tem o objetivo de obter a autenticação para usarmos a API Google. 
-Para ela passamos número do projeto e o id da secret para obter a secret por meio de 'access_secret' e depois realizar a autenticação.
+
 EX:
-    credentials = get_auth(project_number, secret)
+    credentials = get_auth(project_number)
 '''
 
-def get_auth(project_number, secret_id):
+def get_auth(project_number):
     try:
-        service_account_json = access_secret(project_number, secret_id)
+        # Obtendo credenciais padrão
+        credentials, project = default()
 
-        # Convertendo o JSON da chave da conta de serviço em um dicionário
-        service_account_info = json.loads(service_account_json)
+        # Renova as credenciais
+        if credentials.expired and credentials.refresh_token:
+            credentials.refresh(Request())
 
-        # Autenticação usando a chave da conta de serviço recuperada
-        credentials = service_account.Credentials.from_service_account_info(
-            service_account_info, scopes=["https://www.googleapis.com/auth/cloud-platform"]
-        )
-        print("Sucesso ao obter autenticação!")
+        print(f"Autenticação bem-sucedida para o projeto {project_number}")
         return credentials
 
+    except exceptions.DefaultCredentialsError as e:
+        print(f"Erro ao obter credenciais: {e}")
+        return None
     except Exception as e:
-        print(f'Erro: {e}')
-        return e
+        print(f'Erro no Auth.py: {e}')
+        return None
