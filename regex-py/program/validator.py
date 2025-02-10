f41070eeb4124da0d37897646b65cbb475d8fedb
Giovani Rodrigues <giovanirodrigues021@gmail.com>
2025-02-10 15:45:01 +0000

    feat: added validations

--- regex-py/program/validator.py
+++ regex-py/program/validator.py
@@ -1,22 +1,50 @@
 import re
-from files import get_content
+import os
+from files import get_content, load_files
 
-content = get_content("../definitions/", "acordo.sqlx")
+def validation_exists(content):
+    # Itens a validar
+    items = ['partitionBy', 'requirePartitionFilter','@@query_label']
+    
+    for i in items:
+        result = re.search(i, content)
+        if(result):
+            print("{}: OK".format(i))
+        else:
+            raise Exception('Erro: "{}" não encontrado no arquivo'.format(i))
 
-def regex_validate():
-    content = get_content("../definitions/", "acordo.sqlx")
-    print(content)
-    result = re.search('@@query_label', content)
-    return result
+# def validate_requirePartitionFilter_true(content):
+#     result = re.search(i, content)
 
-def validate_query_label(content):
-    result = re.search('@@query_label', content)
-    if (result):
-        print("Query label: OK")
-    else:
-        print("Query label: ERROR")
-        return exit 
+def exec_validations(content):
+    try:
+        validation_exists(content)
+    except Exception as e:
+        return e
+
 
 if __name__ == "__main__":
-    print(regex_validate())
+    # Carregando diretório Definitions
+    definitions = os.getenv("SOURCE_FOLDER")
+    print(definitions)
+    
+    # Carregando arquivos
+    files, size = load_files(definitions)
+    print('Quantidade de arquivos: {}'.format(size))
+    print(files)
 
+    ok = []
+    for file in files:
+        # Obtendo conteudo
+        file_content = get_content(definitions, file)
+
+        # Validações
+        result = exec_validations(file_content)
+
+        if (result == None):
+            print("Não houve erros: {}\n".format(file))
+        else:
+            print("Houve um erro: {} - {}\n".format(file, result))
+            ok.append(result)
+    
+    
