51af61494b198c49841bb9410672d6546aeaeb9e
Giovani Rodrigues <giovanirodrigues021@gmail.com>
2025-02-12 15:12:08 +0000

    refactor: create validations.py

--- regex-py/program/main.py
+++ regex-py/program/main.py
@@ -1,55 +1,7 @@
+import os        
+from validations import exec_validations
 import re
-import os
 from files import get_content, load_files
-
-def validation_exists(content):
-    # Itens a validar
-    items = ['partitionBy','requirePartitionFilter', '@@query_label']
-    
-    for i in items:
-        result = re.search(i, content)
-        if(result):
-            print(" -> {}: OK".format(i))
-        else:
-            raise Exception('Erro: "{}" não encontrado no arquivo'.format(i))
-
-def validate_requirePartitionFilter_true(content):
-    exp = "requirePartitionFilter"
-    result = re.search(r'requirePartitionFilter:\s*true', content, re.IGNORECASE)
-    if (result):
-        print("requirePartitionFilter definido como true")
-    else:
-        raise Exception('Erro: requirePartitionFilter não foi definido como true')
-
-def validate_sql_command(content):
-    # Define padrão
-    pattern = r"pre_operations\s*{.*?}$"
-
-    # busca a incidencia no conteudo
-    match = re.search(pattern, content, re.DOTALL)
-
-    # Processa o conteudo obtido
-    if match:
-        # obter codigo
-        sql_code = match.group() 
-        lines = sql_code.splitlines()
-
-        # remove linhas desnecessárias
-        sql_cleaned = '\n'.join(lines[1:-1])
-            
-        # Grava arquivo para realizar teste
-        with open("test.sql", "w", encoding="utf-8") as file:
-            file.write(sql_cleaned)
-            print("Gravado com sucesso")    
-        
-
-def exec_validations(content):
-    try:
-        validation_exists(content)
-        validate_requirePartitionFilter_true(content)
-        validate_sql_command(content)
-    except Exception as e:
-        return e
     
 variabels_words = {
     "BETWEEN",
@@ -98,13 +50,12 @@ def partition_exist(file):
 
 if __name__ == "__main__":
     # Carregando diretório Definitions
-    definitions = os.getenv("SOURCE_FOLDER")
-    print(definitions)
+    definitions = os.getenv("SOURCE_FOLDER")    
     
     # Carregando arquivos
     files, size = load_files(definitions)
     print('Quantidade de arquivos: {}'.format(size))
-    print(files)
+    print("{}\n".format(files))
 
     ok = []
     for file in files:
@@ -112,12 +63,12 @@ if __name__ == "__main__":
         file_content = get_content(definitions, file)
 
         # Validações
-        result = exec_validations(file_content)
+        result = exec_validations(file_content, file)
 
         if (result == None):
-            print("Não houve erros: {}\n".format(file))
+            print("Sem erros em \033[33m {} \033[0m\n".format(file))
         else:
-            print("Houve um erro em: {} - {}\n".format(file, result))
+            print("Erro em \033[33m {} \033[0m - {}\n".format(file, result))
             ok.append(result)
 
     find_name("dataset","processo",file_content)
---
+++ regex-py/program/validations.py
@@ -0,0 +1,86 @@
+import re
+
+
+"""
+    OBJETIVO: validar se há presença de palavra-chave
+    PARÂMETROS: Recebe uma string como conteudo e outra 
+                como nome do arquivo que serão usados para
+                verificar ocorrências e notificar o nome do arquivo verificado
+    Ex:
+        validation_exists(content, file_name)
+"""
+def validation_exists(content, file_name):
+    # Itens a validar
+    print("Iniciando validação em : {}".format(file_name))
+    items = ['partitionBy','requirePartitionFilter', '@@query_label']
+    
+    for i in items:
+        result = re.search(i, content)
+        if(result):
+            print(" -> {}: \033[33mOK\033[0m".format(i))
+        else:
+            raise Exception('Erro: "{}" não encontrado no arquivo'.format(i))
+
+"""
+    OBJETIVO: validar se há presença de palavra-chave e se foi definida como TRUE
+    PARÂMETROS: Recebe uma string como conteudo usada para
+                verificar ocorrência e validar se está definida como TRUE
+    Ex:
+        validate_requirePartitionFilter_true(content)
+"""
+def validate_requirePartitionFilter_true(content):
+    exp = "requirePartitionFilter"
+    result = re.search(r'requirePartitionFilter:\s*true', content, re.IGNORECASE)
+    if (result):
+        print(" -> requirePartitionFilter definido como \033[33mTRUE\033[0m\n")
+    else:
+        raise Exception('Erro: requirePartitionFilter não foi definido como true')
+
+"""
+    OBJETIVO: criar arquivos .sql que serão avaliados pelo SQL Fluff na pipeline
+    PARÂMETROS: Recebe uma string como conteudo e outra 
+                como nome do arquivo que serão usados para
+                criar o arquivo
+    Ex:
+        create_sql_for_validate(content, file_name)
+"""
+def create_sql_for_validate(content, file_name):
+    # Define padrão
+    pattern = r"pre_operations\s*{.*?}$"
+
+    # busca a incidencia no conteudo
+    match = re.search(pattern, content, re.DOTALL)
+
+    # Processa o conteudo obtido
+    if match:
+        # obter codigo
+        sql_code = match.group() 
+        lines = sql_code.splitlines()
+
+        # remove linhas desnecessárias
+        sql_cleaned = '\n'.join(lines[1:-1])
+            
+
+        file_name = file_name.replace("sqlx", "sql")
+        path = './sql_files_for_tests/{}'.format(file_name)
+
+        # Grava arquivo para realizar teste
+        with open(path, "w", encoding="utf-8") as file:
+            file.write(sql_cleaned)
+            print("Gravado com Sucesso:\033[33m {} \033[0m".format(file_name))    
+            
+
+"""
+    OBJETIVO: centralizar e executar funções de validação. Caso haja erro, retorna uma exceção
+    PARÂMETROS: Recebe uma string como conteudo e outra como nome do arquivo
+    Ex:
+        result = exec_validations(content, file_name)
+
+"""
+def exec_validations(content, file_name):
+    try:
+        validation_exists(content, file_name)
+        validate_requirePartitionFilter_true(content)
+        create_sql_for_validate(content, file_name)
+    except Exception as e:
+        return e
