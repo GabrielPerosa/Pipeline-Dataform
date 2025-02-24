fa2108c2d2e28b874a2837e5d8d2a9aac71ce8bf
Gabriel Perosa <gabrielperosa493@gmail.com>
2025-02-24 18:37:24 +0000

    feat: add validations

---
+++ regex-py/program/test.sql
@@ -0,0 +1,186 @@
+  DECLARE nom_processo STRING DEFAULT "cobranca_acordo.sqlx";
+  DECLARE nom_tabela STRING DEFAULT "cobranca_acordo";
+  DECLARE dat_ini_movimento DATE;
+  DECLARE dat_fim_movimento DATE;
+  DECLARE dth_ult_data_processada TIMESTAMP;
+  DECLARE dth_inicio_execucao TIMESTAMP;
+  DECLARE atual_ult_data_processada TIMESTAMP;
+  DECLARE before_rows_count INT64;
+  DECLARE after_rows_count INT64;
+  SET @@query_label = "routine:cobranca_acordo";
+  CALL integracaohomologado.corp_gestao_processamento.get_processo_log(
+    nom_processo,
+    nom_tabela,
+    dat_ini_movimento,
+    dat_fim_movimento,
+    dth_ult_data_processada,
+    dth_inicio_execucao
+  );
+  SET before_rows_count = (
+    SELECT
+      row_count
+    FROM
+      ${ref("__TABLES__")}
+    WHERE
+      table_id = 'cobranca_acordo'
+  );
+}
+WITH acordo AS (
+  SELECT
+    id AS id_acordo_cobranca,
+    cliente AS id_cliente_externo,
+    cobrador AS id_cobrador,
+    tipo AS nom_modalidade_acordo,
+    numeroAcordo AS num_acordo,
+    numeroParcelas AS num_parcelas,
+    dataOperacao AS dat_operacao,
+    dataEmissao AS dat_emissao,
+    dataProcessamento AS dth_processamento,
+    dataHoraInclusao AS dth_inclusao_origem,
+    dataHoraModificacao AS dth_alteracao_origem,
+    dataVencimento AS dat_vencimento,
+    situacao AS ind_situacao,
+    taxaOperacao AS val_taxa_operacao,
+    valorPrincipal AS val_principal,
+    valorJuros AS val_juros,
+    valorPagoTributo AS val_atributo,
+    valorTotal AS val_total,
+    saldoPrincipal AS val_saldo_principal,
+    saldoTotal AS val_saldo_total,
+    saldoAtual AS val_saldo_atual,
+    diasAtraso AS qtd_dias_atraso,
+    production_date AS dat_referencia
+  FROM
+    ${ref('pfs_risco_raw_tivea', 'acordo')}
+  WHERE
+    production_date BETWEEN dat_ini_movimento AND dat_fim_movimento
+),
+acordo_linha AS (
+  SELECT *, ROW_NUMBER() OVER (PARTITION BY id_cliente_externo, num_acordo, dat_referencia ORDER BY COALESCE(CAST(qtd_dias_atraso AS INT64), 0)
+ DESC) AS rn
+  FROM acordo
+),
+cliente AS (
+    SELECT
+        num_cpf_cnpj_cliente,
+        id_cliente_externo,
+        dat_referencia
+    FROM
+        ${ref('pfs_risco_tivea', 'cobranca_cliente')}
+    WHERE
+        dat_referencia BETWEEN dat_ini_movimento AND dat_fim_movimento
+),
+origem_acordo AS (
+    SELECT
+        dat_referencia,
+        id_acordo_cobranca,
+        dat_vencimento AS dat_atraso_orig_acordo,
+        COALESCE(CAST(val_desconto_total AS INT64), 0) + COALESCE(CAST(val_desc_permanencia AS INT64), 0) AS val_desconto_total,
+        ROW_NUMBER() OVER (PARTITION BY id_acordo_cobranca, dat_referencia ORDER BY num_ordem_contrato DESC) AS rn
+    FROM
+        integracaohomologado.pfs_risco_tivea.cobranca_origem_acordo
+    WHERE
+        dat_referencia BETWEEN dat_ini_movimento AND dat_fim_movimento
+),
+neg AS (
+    SELECT
+        id AS id_acordo_cobranca,
+        negociacao.nome AS nom_acordo_negociacao,
+        negociacao.id AS id_acordo_negociacao,
+        negociacao.modalidade.nome AS nom_modalidade_acordo,
+        production_date AS dat_referencia
+    FROM
+        ${ref('pfs_risco_raw_tivea', 'acordo')}
+    WHERE
+        production_date BETWEEN dat_ini_movimento AND dat_fim_movimento
+),
+tip_pag_assess_user AS (
+    SELECT
+        id AS id_acordo_cobranca,
+        meioPagamento.id AS id_acordo_pagto,
+        meioPagamento.tipo AS tip_acordo_meio_pagto,
+        meioPagamento.cobrador.nome AS nom_cobrador,
+        assessoria.id AS id_acordo_assessoria,
+        assessoria.nome AS nom_acordo_assessoria,
+        usuario.id AS id_acordo_usuario,
+        usuario.nome AS nom_acordo_usuario,
+        production_date AS dat_referencia
+    FROM
+        ${ref('pfs_risco_raw_tivea', 'acordo')}
+    WHERE
+        production_date BETWEEN dat_ini_movimento AND dat_fim_movimento
+)
+SELECT
+    CAST(aco.id_acordo_cobranca AS INT64) AS id_acordo_cobranca,
+    CAST(aco.id_cliente_externo AS INT64) AS id_cliente_externo,
+    cli.num_cpf_cnpj_cliente,
+    aco.id_cobrador,
+    tip.nom_cobrador,
+    neg.nom_modalidade_acordo AS tip_modalidade_acordo,
+    CAST(aco.num_acordo AS INT64) AS num_acordo,
+    CAST(aco.num_parcelas AS INT64) AS num_parcelas,
+    CAST(aco.dat_operacao AS TIMESTAMP) AS dat_operacao,
+    CAST(aco.dat_emissao AS TIMESTAMP) AS dat_emissao,
+    CAST(aco.dth_processamento AS TIMESTAMP) AS dth_processamento,
+    CAST(aco.dth_inclusao_origem AS TIMESTAMP) AS dth_inclusao_origem,
+    CAST(aco.dth_alteracao_origem AS TIMESTAMP) AS dth_alteracao_origem,
+    CAST(aco.dat_vencimento AS DATE) AS dat_vencimento,
+    aco.ind_situacao,
+    CAST(aco.val_taxa_operacao AS INT64) AS val_taxa_operacao,
+    CAST(aco.val_principal AS INT64) AS val_principal,
+    CAST(aco.val_juros AS INT64) AS val_juros,
+    CAST(aco.val_atributo AS INT64) AS val_atributo,
+    CAST(aco.val_total AS INT64) AS val_total,
+    CAST(ori.val_desconto_total AS INT64) AS val_desconto,
+    CAST(aco.val_saldo_principal AS INT64) AS val_saldo_principal,
+    CAST(aco.val_saldo_total AS INT64) AS val_saldo_total,
+    CAST(aco.val_saldo_atual AS INT64) AS val_saldo_atual,
+    CAST(aco.qtd_dias_atraso AS INT64) AS qtd_dias_atraso,
+    CAST(ori.dat_atraso_orig_acordo AS DATE) AS dat_atraso_orig_acordo,
+    CAST(tip.id_acordo_usuario AS INT64) AS id_acordo_usuario,
+    tip.nom_acordo_usuario,
+    CAST(tip.id_acordo_assessoria AS INT64) AS id_acordo_assessoria,
+    tip.nom_acordo_assessoria,
+    CAST(neg.id_acordo_negociacao AS INT64) AS id_acordo_negociacao,
+    neg.nom_acordo_negociacao,
+    tip.tip_acordo_meio_pagto,
+    aco.dat_referencia
+FROM
+    acordo_linha aco
+LEFT JOIN cliente cli ON aco.id_cliente_externo = cli.id_cliente_externo AND aco.dat_referencia = cli.dat_referencia AND aco.rn = 1
+LEFT JOIN origem_acordo ori ON CAST(aco.id_acordo_cobranca AS STRING) = CAST(ori.id_acordo_cobranca AS STRING) AND aco.dat_referencia = ori.dat_referencia AND ori.rn = 1
+LEFT JOIN neg neg ON aco.id_acordo_cobranca = neg.id_acordo_cobranca AND aco.dat_referencia = neg.dat_referencia
+LEFT JOIN tip_pag_assess_user tip ON aco.id_acordo_cobranca = tip.id_acordo_cobranca AND aco.dat_referencia = tip.dat_referencia
+WHERE aco.rn = 1
+post_operations {
+  SET after_rows_count = (
+    SELECT
+      row_count
+    FROM
+      ${ref("__TABLES__")}
+    WHERE
+      table_id = 'cobranca_acordo'
+  );
+  SET atual_ult_data_processada = (
+    SELECT
+      max(dth_processamento)
+    FROM
+      ${self()}
+    WHERE
+      dth_processamento >= dth_ult_data_processada
+      AND dat_referencia BETWEEN dat_ini_movimento AND dat_fim_movimento
+    LIMIT 1
+  );
+  CALL integracaohomologado.corp_gestao_processamento.insert_processo_log(
+    nom_processo,
+    nom_tabela,
+    dat_ini_movimento,
+    dat_fim_movimento,
+    atual_ult_data_processada,
+    dth_inicio_execucao,
+    (
+      SELECT
+        after_rows_count - before_rows_count AS count
+    ),
+    "EXECUÇÃO FINALIZADA COM SUCESSO"
+  );
--- regex-py/program/validations.py
+++ regex-py/program/validations.py
@@ -1,156 +1,131 @@
 import re
 import os
 
-"""
-    OBJETIVO:   Validar se há presença de palavra-chaves especificadas
-    
-    PARÂMETROS: Recebe uma string como conteudo e outra 
-                como nome do arquivo que serão usados para
-                verificar ocorrências
-    Ex:
-        validation_if_exists(content)
-"""
 def validation_if_exists(content):    
-    # type do script
-    type_config =  validate_type_in_config(content)
+    type_config = validate_type_in_config(content)
     
-    if (type_config == 'incremental'):
-        print("--> Type definido para: \33[33m{}\33[0m".format(type_config))
-        
+    if type_config == 'incremental':
+        print(f"--> Type definido para: \33[33m{type_config}\33[0m")
         items = ['updatePartitionFilter', 'uniqueKey']
         for i in items:
             result = re.search(i, content)
-            if (result):
-                print("--> {}: \033[33mOK\033[0m".format(i))
+            if result:
+                print(f"--> {i}: \033[33mOK\033[0m")
             else:
-                raise Exception('Erro: "{}" não encontrado no arquivo'.format(i))
+                raise Exception(f'Erro: "{i}" não encontrado no arquivo')
     else:
-        print("\33[33mType não definido como incremental:{}\33[0m".format(type))
+        print(f"\33[33mType não definido como incremental: {type_config}\33[0m")
 
-    # Itens a validar
     item = '@@query_label'
     result = re.search(item, content)
-    
-    if(result):
-        print("--> {}: \033[33mOK\033[0m".format(item))
+    if result:
+        print(f"--> {item}: \033[33mOK\033[0m")
     else:
-        raise Exception('Erro: "{}" não encontrado no arquivo'.format(i))
+        raise Exception(f'Erro: "{item}" não encontrado no arquivo')
 
-"""
-    OBJETIVO: Validar se foi definida a partição da view e verificar se foi usada no código SQL
-    PARÂMETROS: o conteudo que passará por validação
-    Ex: 
-        validate_partitionDefinition(content)
-        
-"""
 def validate_partitionDefinition(content):
-    # Primeira parte da validação
     requirePartitionFilter = r'requirePartitionFilter:\s*true'
     result = re.search(requirePartitionFilter, content, re.IGNORECASE)
-    if (result):
+    if result:
         print("--> requirePartitionFilter definido como \033[33mTRUE\033[0m")
     else:
         raise Exception('Erro: requirePartitionFilter não foi definido como true')
 
-    # Segunda parte da validação
     partitionBy = r'partitionBy:\s*"([^"]+)",'
     result = re.search(partitionBy, content)
-
-    if(result):
+    if result:
         partition_name = result.group(1)
-        print("--> partitionBy foi definido: \033[33m{} \033[0m ".format(partition_name))
+        print(f"--> partitionBy foi definido: \033[33m{partition_name}\033[0m")
         
-        partitionBy = r'PARTITION BY\s+(.*)'
-        partition_name_in_sql = re.search(partitionBy, content, re.IGNORECASE)
-        
-        if not (result):
+        partitionBy_sql = r'PARTITION BY\s+(.*)'
+        partition_name_in_sql = re.search(partitionBy_sql, content, re.IGNORECASE)
+        if not partition_name_in_sql:
             print("\033[33mPartição não está sendo usada no código SQL\033[0m")
-        elif ( 0 > partition_name_in_sql.group(1).find(partition_name)):
+        elif partition_name_in_sql.group(1).find(partition_name) < 0:
             print("\033[33mNome da partição está diferente no código SQL\033[0m")
         else:
             print("--> Partição está sendo usada no código SQL")
     else:
         print("\033[33mpartitionBy não definido\033[0m")
-    
-""" 
-    OBJETIVO: Validar se existe o comando para criar tabela se ela não existir
-    PARÂMETROS: O conteudo que passará por validação
-    Ex: 
-        validate_create_table(content)
-"""
+
 def validate_create_table(content):
-    # Verifica se existe create table if not exists 
     create_table_pattern = r'pre_operations\s*\{\s*CREATE\s+TABLE\s+IF\s+NOT\s+EXISTS'
     result = re.search(create_table_pattern, content, re.IGNORECASE)
-    
-    if (result):
+    if result:
         print("--> CREATE TABLE IF NOT EXISTS em pre_operations\33[33m: OK\033[0m")
     else:
         print("\033[33m--> Não há CREATE TABLE IF NOT EXISTS em pre_operations\033[0m")
 
-"""
-    OBJETIVO: Validar se o tipo da configuração do script é incremental ou não 
-    PARÂMETROS: O conteúdo que passará por validação
-    Ex: 
-        validate_type_in_config(content)
-"""
 def validate_type_in_config(content):
-    # Verifica se type = incremental
     incremental_pattern = r'type:\s*"([^"]+)"'
     result = re.search(incremental_pattern, content)
-    return result.group(1)
+    if result:
+        return result.group(1)
+    raise Exception("Erro: 'type' não definido no arquivo")
 
-
-"""
-    OBJETIVO: criar arquivos .sql que serão avaliados pelo SQL Fluff na pipeline
-    PARÂMETROS: Recebe uma string como conteudo e outra 
-                como nome do arquivo que serão usados para
-                criar o arquivo
-    Ex:
-        create_sql_for_validate(content, file_name)
-"""
 def create_sql_for_validate(content, file_name):
     sql_folder = './sql_files_for_tests'
-
-    # Padrão para encontrar código SQL
     pattern = r"pre_operations\s*{.*?}$"
-
-    # busca a incidencia no conteudo
     match = re.search(pattern, content, re.DOTALL)
     if match:
-        # obter codigo
-        sql_code = match.group() 
+        sql_code = match.group()
         lines = sql_code.splitlines()
-
-        # remove linhas desnecessárias
         sql_cleaned = '\n'.join(lines[1:-1])
-            
-        # Cria a pasta para armazenar SQLs
+        
         if not os.path.exists(sql_folder):
             os.makedirs(sql_folder)
-
+        
         file_name = file_name.replace("sqlx", "sql")
-        path = './sql_files_for_tests/{}'.format(file_name)
-
-        # Grava arquivo para realizar teste
+        path = f'./sql_files_for_tests/{file_name}'
         with open(path, "w", encoding="utf-8") as file:
             file.write(sql_cleaned)
-            print("Gravado com Sucesso:\033[33m {} \033[0m".format(file_name))    
-            
+            print(f"Gravado com Sucesso:\033[33m {file_name} \033[0m")
 
 
-"""
-    OBJETIVO: centralizar e executar funções de validação. Caso haja erro, retorna uma exceção
-    PARÂMETROS: Recebe uma string como conteudo e outra como nome do arquivo
-    Ex:
-        result = exec_validations(content, file_name)
-"""
+def validate_where_clause(content):
+    where_pattern = r'WHERE\s+([^;]*\s*<=\s*CURRENT_DATE\(\)\s*[^;]*)'
+    where_matches = re.findall(where_pattern, content, re.IGNORECASE)
+    
+    if not where_matches:
+        print("\033[33m--> Nenhuma cláusula WHERE <= CURRENT_DATE encontrada no script\033[0m")
+        return
+    
+    print(f"--> Cláusulas WHERE <= CURRENT_DATE encontradas: \033[33m{len(where_matches)}\033[0m")
+    
+    
+def validate_where_clause2(content):
+    where_pattern = r'WHERE\s+([^;]*\s*<\s*CURRENT_DATE\(\)\s*[^;]*)'
+    where_matches = re.findall(where_pattern, content, re.IGNORECASE)
+    
+    if not where_matches:
+        print("\033[33m--> Nenhuma cláusula WHERE < CURRENT_DATE encontrada no script\033[0m")
+        return
+    
+    print(f"--> Cláusulas WHERE < CURRENT_DATE encontradas: \033[33m{len(where_matches)}\033[0m")
+
+
+def validate_where_clause3(content):
+    where_pattern = r'WHERE\s+([^;]*\s*<=\s*CURRENT_TIMESTAMP()\(\)\s*[^;]*)'
+    where_matches = re.findall(where_pattern, content, re.IGNORECASE)
+    
+    if not where_matches:
+        print("\033[33m--> Nenhuma cláusula WHERE <= CURRENT_TIMESTAMP() encontrada no script\033[0m")
+        return
+    
+    print(f"--> Cláusulas WHERE <= CURRENT_TIMESTAMP() encontradas: \033[33m{len(where_matches)}\033[0m")
+
+
+
 def exec_validations(content, file_name):
     try:
-        print("Iniciando validação em : {}".format(file_name))
+        print(f"Iniciando validação em: {file_name}")
+        validate_where_clause(content)
+        validate_where_clause2(content)
+        validate_where_clause3(content)
         validate_partitionDefinition(content)
         validate_create_table(content)
         validation_if_exists(content)
         create_sql_for_validate(content, file_name)
     except Exception as e:
         return e
+
