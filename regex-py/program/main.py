9b7ae0fc19f0c46887dd19329fd101ce764c12c5
Henrique Fernandes <henriquealcalde@hotmail.com>
2025-02-25 15:39:43 -0300

    docs:reorganize the docstring

--- regex-py/program/main.py
+++ regex-py/program/main.py
@@ -3,25 +3,24 @@ from validations import exec_validations
 import re
 from files import get_content, load_files
 
-if __name__ == "__main__":
     # Carregando diretório Definitions
-    definitions = os.getenv("SOURCE_FOLDER")    
-    
-    # Carregando arquivos
-    files, size = load_files(definitions)
-    print('Quantidade de arquivos: {}'.format(size))
-    print("{}\n".format(files))
+definitions = os.getenv("SOURCE_FOLDER")    
 
-    ok = []
-    for file in files:
-        # Obtendo conteudo
-        file_content = get_content(definitions, file)
+# Carregando arquivos
+files, size = load_files(definitions)
+print('Quantidade de arquivos: {}'.format(size))
+print("{}\n".format(files))
 
-        # Validações
-        result = exec_validations(file_content, file)
+ok = []
+for file in files:
+    # Obtendo conteudo
+    file_content = get_content(definitions, file)
 
-        if (result == None):
-            print("\033[31m ___ Sem erros em  {} ___\033[0m\n".format(file))
-        else:
-            print("Erro em \033[33m {} \033[0m - {}\n".format(file, result))
-            ok.append(result)
+    # Validações
+    result = exec_validations(file_content, file)
+
+    if (result == None):
+        print("\033[31m ___ Sem erros em  {} ___\033[0m\n".format(file))
+    else:
+        print("Erro em \033[33m {} \033[0m - {}\n".format(file, result))
+        ok.append(result)
